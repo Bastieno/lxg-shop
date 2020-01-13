0ee48cb94f07ba5076f35be1aebfa11a0baafda5
Francis Nduamaka <francis.nduamaka@gmail.com>
2020-01-13 21:48:04 +0100

    create the app homepage

--- lxgShop/urls.py
+++ lxgShop/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api.models import ProductResource
+from . import views
 
 product_api = ProductResource()
 
 urlpatterns = [
+    path('', views.home),
     path('shop/', include('shop.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/', include(product_api.urls))
