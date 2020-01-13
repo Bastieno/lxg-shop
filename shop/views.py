9b56338d211600879fc3013f29e175373a9ee977
Francis Nduamaka <francis.nduamaka@gmail.com>
2020-01-13 17:57:04 +0100

    create a template for the index page

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 
 def index(request):
-  return HttpResponse('Hello, You are at the movies home page')
+  products = Product.objects.all()
+  # output = ', '.join([product.name for product in products])
+  return render(request, 'shop/index.html', {'products': products })
